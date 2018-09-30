d973b8a3c278d1d5c51a48fbb812895ebc84d53f
jwang1 <sigkdd@yahoo.com>
2018-09-30 15:57:51 -0400

    1. Addec CannonView 2. add cannonView in cannonFragment 3. added the found wave files in new raw dir

---
+++ app/src/main/java/com/iexpress/android/cannongame/CannonView.java
@@ -0,0 +1,10 @@
+package com.iexpress.android.cannongame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class CannonView extends SurfaceView {
+    public CannonView(Context context) {
+        super(context);
+    }
+}
